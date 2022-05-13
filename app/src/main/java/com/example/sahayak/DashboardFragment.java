3e333cfc9930a6a4d5e22f74df12c635e6ae4bae
Hemant Dhankar <hemantdhankar123@gmail.com>
2022-05-13 14:15:07 +0530

    multiple markers on map feed

--- app/src/main/java/com/example/sahayak/DashboardFragment.java
+++ app/src/main/java/com/example/sahayak/DashboardFragment.java
@@ -2,31 +2,76 @@ package com.example.sahayak;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link DashboardFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DashboardFragment extends Fragment {
+public class DashboardFragment extends Fragment implements OnMapReadyCallback {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
+    private GoogleMap mMap;
+    ArrayList<HashMap<String,Object>> issueList = new ArrayList<>();
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     public DashboardFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("Issue_detail")
+                .whereNotEqualTo("Status", "Resolved")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String issue_id = document.getId();
+                                HashMap<String,Object> temp = (HashMap<String, Object>) document.getData();
+                                String c = temp.get("image_path")==null?"": (String) temp.get("image_path");
+
+                                issueList.add(temp);
+                            }
+                            onMapReady(mMap);
+                        } else {
+                            Log.w("Feed_Fragment", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 
     /**
@@ -38,6 +83,7 @@ public class DashboardFragment extends Fragment {
      * @return A new instance of fragment DashboardFragment.
      */
     // TODO: Rename and change types and number of parameters
+
     public static DashboardFragment newInstance(String param1, String param2) {
         DashboardFragment fragment = new DashboardFragment();
         Bundle args = new Bundle();
@@ -69,6 +115,25 @@ public class DashboardFragment extends Fragment {
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.MainContentFragment,new Feed_Fragment()).commit();
             }
         });
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         return v;
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        for(HashMap<String, Object> item : issueList){
+            LatLng latlng = new LatLng((double)item.get("latitude"), (double)item.get("longitude"));
+            mMap.addMarker(new MarkerOptions()
+                    .position(latlng)
+                    .title((String) item.get("title")));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng,15));
+        }
+
+
     }
 }
