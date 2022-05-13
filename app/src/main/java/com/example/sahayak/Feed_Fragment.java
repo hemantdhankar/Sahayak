01988bb898dfbc29ebcb47523f345f773105b475
nitesh18400 <nitesh18400@iiitd.ac.in>
2022-05-13 13:26:11 +0530

    map feed fragment added

--- app/src/main/java/com/example/sahayak/Feed_Fragment.java
+++ app/src/main/java/com/example/sahayak/Feed_Fragment.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -113,8 +114,16 @@ public class Feed_Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         view=inflater.inflate(R.layout.fragment_feed, container, false);
+        Button feed_view_button=view.findViewById(R.id.map_feed);
+        feed_view_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.MainContentFragment,new DashboardFragment()).commit();
+            }
+        });
         ArrayList<feedItem> feed_arr=new ArrayList<>();
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         feed_arr.clear();
